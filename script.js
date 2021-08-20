4266cb497ebd3f240c2679c42697a7d48beae578
Davi Paulo Jessé da Silva <davipaulodobrasil2@gmail.com>
2021-08-20 20:59:14 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function colorir(){
+    var cor1 = window.document.getElementById('c1')
+    var cor2 = cor1.value
+    var r = window.document.getElementById('main');
+    r.style.backgroundColor = cor2
+}
